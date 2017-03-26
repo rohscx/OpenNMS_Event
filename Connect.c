f65dd657a9b0f8577d7975108ef08f1a5826f6b2
rohscx <rohscx@gmail.com>
2017-03-26 08:36:12 -0400

    Update Connect.c

--- Connect.c
+++ Connect.c
@@ -14,7 +14,7 @@
 
 #include <arpa/inet.h>
 
-#define PORT "80" // the port client will be connecting to 
+#define PORT "53" // the port client will be connecting to 
 
 #define MAXDATASIZE 100 // max number of bytes we can get at once 
 
