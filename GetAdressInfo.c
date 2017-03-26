a3cc4867bb8c47298b1189acfbdc29cbef418757
rohscx <rohscx@gmail.com>
2017-03-26 07:55:31 -0400

    Create GetAdressInfo.c

---
+++ GetAdressInfo.c
@@ -0,0 +1,8 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+int getaddrinfo(const char *node,     // e.g. "www.example.com" or IP
+                const char *service,  // e.g. "http" or port number
+                const struct addrinfo *hints,
+                struct addrinfo **res);
