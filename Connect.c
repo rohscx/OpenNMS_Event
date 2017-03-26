9f2ec5656f3da6c6d521c07a37c4349e404f6647
rohscx <rohscx@gmail.com>
2017-03-26 08:20:04 -0400

    Update Connect.c

--- Connect.c
+++ Connect.c
@@ -7,7 +7,7 @@ memset(&hints, 0, sizeof hints);
 hints.ai_family = AF_UNSPEC;
 hints.ai_socktype = SOCK_STREAM;
 
-getaddrinfo("www.example.com", "3490", &hints, &res);
+getaddrinfo("google.com", "80", &hints, &res);
 
 // make a socket:
 
