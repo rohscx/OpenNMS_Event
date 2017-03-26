f5db1c3bf0fb36db9019fae2f4bf14f3bbb9825a
rohscx <rohscx@gmail.com>
2017-03-26 08:19:12 -0400

    Create Connect.c

---
+++ Connect.c
@@ -0,0 +1,18 @@
+struct addrinfo hints, *res;
+int sockfd;
+
+// first, load up address structs with getaddrinfo():
+
+memset(&hints, 0, sizeof hints);
+hints.ai_family = AF_UNSPEC;
+hints.ai_socktype = SOCK_STREAM;
+
+getaddrinfo("www.example.com", "3490", &hints, &res);
+
+// make a socket:
+
+sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+// connect!
+
+connect(sockfd, res->ai_addr, res->ai_addrlen);
